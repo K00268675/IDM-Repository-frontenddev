f637d3b5ae517b74370ff05cf75de3fe4fabeb67
K00268675 <114759005+K00268675@users.noreply.github.com>
2022-10-19 10:19:10 +0100

    Add files via upload

---
+++ Quote Generator/quotes.js
@@ -0,0 +1,84 @@
+const localQuotes = [
+    {
+      text: "Green Beans, Potatoes, Tomatoes.",
+      author: "Unknown"
+    },
+    {
+      text: "Beans, Rice, Jesus Christ and Byron.",
+      author: "Unknown"
+    },
+     {
+      text: "It's a hard hat holiday, respectfully.",
+      author: "Unknown"
+    },
+     {
+      text: "You've been Trolled.",
+      author: "Unknown"
+    },
+    {
+        text: "Grass Tastes Bad.",
+        author: "Unknown"
+      },
+      {
+        text: "2+2=5.",
+        author: "Unknown"
+      },
+      {
+        text: "Putting chemicals in the water that turn the frogs gay.",
+        author: "Unknown"
+      },
+      {
+        text: "Go touch grass.",
+        author: "Unknown"
+      },
+      {
+        text: "Don't forget to drink some water.",
+        author: "Unknown"
+      },
+      {
+        text: "Eat healthier ya slob.",
+        author: "Unknown"
+      },
+      {
+        text: "Help",
+        author: "Unknown"
+      },
+      {
+        text: "Get dunked on.",
+        author: "Unknown"
+      },
+      {
+        text: "Hehehehehehehehehhe.",
+        author: "Unknown"
+      },
+      {
+        text: "Skill Issue.",
+        author: "Unknown"
+      },
+      {
+        text: "Bot Diff.",
+        author: "Unknown"
+      },
+      {
+        text: "spawn die spawn die spawn die spawn die.",
+        author: "Unknown"
+      },
+      {
+        text: "Ah stop, I could've dropped my croissant.",
+        author: "Unknown"
+      },
+      {
+        text: "Only a Spoonful.",
+        author: "Unknown"
+      },
+      {
+        text: "*Taco Bell Ding Noise*.",
+        author: "Unknown"
+      },
+      {
+        text: "You know when you've been Tangoed.",
+        author: "Unknown"
+      }
+    
+  ]
+  

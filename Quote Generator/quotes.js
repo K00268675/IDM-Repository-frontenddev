d5563759cdf8e3bcf2c3ecb7f67d6ca96762394d
K00268675 <114759005+K00268675@users.noreply.github.com>
2022-10-19 10:39:43 +0100

    Add files via upload

--- Quote Generator/quotes.js
+++ Quote Generator/quotes.js
@@ -1,83 +1,83 @@
 const localQuotes = [
     {
       text: "Green Beans, Potatoes, Tomatoes.",
-      author: "Unknown"
+      author: "Random guy"
     },
     {
       text: "Beans, Rice, Jesus Christ and Byron.",
-      author: "Unknown"
+      author: "Random guy 2"
     },
      {
       text: "It's a hard hat holiday, respectfully.",
-      author: "Unknown"
+      author: "New Jersey Construction Worker"
     },
      {
       text: "You've been Trolled.",
-      author: "Unknown"
+      author: "Online person"
     },
     {
         text: "Grass Tastes Bad.",
-        author: "Unknown"
+        author: "Rick Sanchez"
       },
       {
         text: "2+2=5.",
-        author: "Unknown"
+        author: "Me"
       },
       {
         text: "Putting chemicals in the water that turn the frogs gay.",
-        author: "Unknown"
+        author: "Alex Jones"
       },
       {
         text: "Go touch grass.",
-        author: "Unknown"
+        author: "Adam the Swedish guy"
       },
       {
         text: "Don't forget to drink some water.",
-        author: "Unknown"
+        author: "Me"
       },
       {
         text: "Eat healthier ya slob.",
-        author: "Unknown"
+        author: "Me to myself"
       },
       {
         text: "Help",
-        author: "Unknown"
+        author: "Also Me"
       },
       {
         text: "Get dunked on.",
-        author: "Unknown"
+        author: "Sans"
       },
       {
         text: "Hehehehehehehehehhe.",
-        author: "Unknown"
+        author: "Adam the Swedish guy"
       },
       {
         text: "Skill Issue.",
-        author: "Unknown"
+        author: "D.Va"
       },
       {
         text: "Bot Diff.",
-        author: "Unknown"
+        author: "The Jungler"
       },
       {
         text: "spawn die spawn die spawn die spawn die.",
-        author: "Unknown"
+        author: "Jarvis"
       },
       {
         text: "Ah stop, I could've dropped my croissant.",
-        author: "Unknown"
+        author: "Vine man"
       },
       {
         text: "Only a Spoonful.",
-        author: "Unknown"
+        author: "King Bach"
       },
       {
         text: "*Taco Bell Ding Noise*.",
-        author: "Unknown"
+        author: "Taco Bell"
       },
       {
         text: "You know when you've been Tangoed.",
-        author: "Unknown"
+        author: "Tango Ad man"
       }
     
   ]
